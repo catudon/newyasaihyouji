c33335233edc0736e825abea3cea283a8b3c0ed3
jitsuwa_udonha <udoncatnyan@gmail.com>
2025-07-14 14:48:46 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+// data.js
+const farmers = [
+  { id: 1, name: "Aさん", profile: "無農薬栽培にこだわってる農家さんだよ〜☆" },
+  { id: 2, name: "Bさん", profile: "珍しい野菜いっぱい育ててるよ〜♪" }
+];
+
+const veggies = [
+  { id: 101, name: "トマト", farmerId: 1, rare: false, recipe: "サラダやパスタに☆", bgImage: "images/130.jpg"},
+  { id: 102, name: "ロマネスコ", farmerId: 2, rare: true, recipe: "蒸してバターで〜♡", bgImage: "images/130.jpg"},
+  { id: 103, name: "空芯菜", farmerId: 2, rare: false, recipe: "ニンニク炒めが定番〜！", bgImage: "images/130.jpg"},
+  {id: 104, name: "トマト", farmerId: 2, rare: false, recipe: "サラダやパスタに☆", bgImage: "images/130.jpg"},
+];
