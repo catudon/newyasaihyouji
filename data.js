f0b9b5bfa980739f7fa4a1ad45529244f7963a3f
jitsuwa_udonha <udoncatnyan@gmail.com>
2025-07-15 23:35:40 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 // data.js
 const farmers = [
-  { id: 1, name: "Aさん", profile: "無農薬栽培にこだわってる農家さんだよ〜☆" },
-  { id: 2, name: "Bさん", profile: "珍しい野菜いっぱい育ててるよ〜♪" }
+  { id: 1, name: "Aさん", profile: "無農薬栽培にこだわってるよ" },
+  { id: 2, name: "Bさん", profile: "珍しい野菜いっぱい育ててるよ" }
 ];
 
 const veggies = [
-  { id: 101, name: "トマト", farmerId: 1, rare: false, recipe: "サラダやパスタに", bgImage: "images/130.jpg"},
-  { id: 102, name: "ロマネスコ", farmerId: 2, rare: true, recipe: "蒸してバターで", bgImage: "images/130.jpg"},
+  { id: 101, name: "トマト", farmerId: 1, rare: false, recipe: "サラダやパスタに", bgImage: "images/fd400518.jpg"},
+  { id: 102, name: "かぼちゃ", farmerId: 2, rare: true, recipe: "煮物に", bgImage: "images/400244.jpg"},
   { id: 103, name: "レタス", farmerId: 2, rare: false, recipe: "回鍋肉に", bgImage: "images/130.jpg"},
-  {id: 104, name: "トマト", farmerId: 2, rare: false, recipe: "サラダやパスタに", bgImage: "images/130.jpg"},
+  {id: 104, name: "トマト", farmerId: 2, rare: false, recipe: "サラダやパスタに", bgImage: "images/fd400525.jpg"},
 ];
