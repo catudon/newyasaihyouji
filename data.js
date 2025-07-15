7b006b8cde2236697e10ab25d1cfa825131ff346
jitsuwa_udonha <udoncatnyan@gmail.com>
2025-07-15 23:43:46 +0900

    Update data.js

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const farmers = [
 
 const veggies = [
   { id: 101, name: "トマト", farmerId: 1, rare: false, recipe: "サラダやパスタに", bgImage: "images/fd400518.jpg"},
-  { id: 102, name: "かぼちゃ", farmerId: 2, rare: true, recipe: "煮物に", bgImage: "images/400244.jpg"},
+  { id: 102, name: "かぼちゃ", farmerId: 2, rare: true, recipe: "煮物に", bgImage: "images/fd400244.jpg"},
   { id: 103, name: "レタス", farmerId: 2, rare: false, recipe: "回鍋肉に", bgImage: "images/130.jpg"},
   {id: 104, name: "トマト", farmerId: 2, rare: false, recipe: "サラダやパスタに", bgImage: "images/fd400525.jpg"},
 ];
