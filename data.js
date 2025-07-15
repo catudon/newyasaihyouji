6771d18aa842aad2373d58de8119e499cc9047b6
jitsuwa_udonha <udoncatnyan@gmail.com>
2025-07-15 18:22:36 +0900

    Update data.js

--- data.js
+++ data.js
@@ -5,8 +5,8 @@ const farmers = [
 ];
 
 const veggies = [
-  { id: 101, name: "トマト", farmerId: 1, rare: false, recipe: "サラダやパスタに☆", bgImage: "images/130.jpg"},
-  { id: 102, name: "ロマネスコ", farmerId: 2, rare: true, recipe: "蒸してバターで〜♡", bgImage: "images/130.jpg"},
-  { id: 103, name: "空芯菜", farmerId: 2, rare: false, recipe: "ニンニク炒めが定番〜！", bgImage: "images/130.jpg"},
-  {id: 104, name: "トマト", farmerId: 2, rare: false, recipe: "サラダやパスタに☆", bgImage: "images/130.jpg"},
+  { id: 101, name: "トマト", farmerId: 1, rare: false, recipe: "サラダやパスタに", bgImage: "images/130.jpg"},
+  { id: 102, name: "ロマネスコ", farmerId: 2, rare: true, recipe: "蒸してバターで", bgImage: "images/130.jpg"},
+  { id: 103, name: "レタス", farmerId: 2, rare: false, recipe: "回鍋肉に", bgImage: "images/130.jpg"},
+  {id: 104, name: "トマト", farmerId: 2, rare: false, recipe: "サラダやパスタに", bgImage: "images/130.jpg"},
 ];
